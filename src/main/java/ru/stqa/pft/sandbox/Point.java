c651392164cd0bd8c2eb9790a6a308868bfd2f7d
cristal20777 <cristal20777@yandex.ru>
2023-02-20 23:36:33 +0300

    новое

--- src/main/java/ru/stqa/pft/sandbox/Point.java
+++ src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -2,12 +2,18 @@ package ru.stqa.pft.sandbox;
 
 public class Point {
  public static void main(String[] args) {
-  double x1 = 9;
-  double y1 = 6;
-  double x2 = 6;
-  double y2 = 1;
+  distance();
+  double x1 = 15;
+  double y1 = 14;
+  double x2 = 3;
+  double y2 = 2;
+  System.out.println("Расстояние между точками = " + distance(double x1,double y1,double x2,double y2));
+ }
+
+ public static void distance(double x1,double y1,double x2,double y2) {
+
   double distance = Math.sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
 
-  System.out.println("Расстояние между точками = " + distance);
+
  }
 }
