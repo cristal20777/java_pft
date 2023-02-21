8b7a9275ab73bf962d73f81347d7bffe4eabaaf3
cristal20777 <cristal20777@yandex.ru>
2023-02-21 19:34:34 +0300

    Задание первое функция и метод

--- src/main/java/ru/stqa/pft/sandbox/Point.java
+++ src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -1,19 +1,17 @@
 package ru.stqa.pft.sandbox;
 
 public class Point {
- public static void main(String[] args) {
-  distance();
-  double x1 = 15;
-  double y1 = 14;
-  double x2 = 3;
-  double y2 = 2;
-  System.out.println("Расстояние между точками = " + distance(double x1,double y1,double x2,double y2));
- }
-
- public static void distance(double x1,double y1,double x2,double y2) {
-
-  double distance = Math.sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
+  public double x;
+  public double y;
 
 
- }
+  //Конструктор
+  public Point(double x, double y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  public double distance(Point p) {
+    return Math.sqrt((this.x - p.x) * (this.x - p.x) + (this.y - p.y) * (this.y - p.y));
+  }
 }
---
+++ src/main/java/ru/stqa/pft/sandbox/Point2.java
@@ -0,0 +1,18 @@
+package ru.stqa.pft.sandbox;
+
+public class Point2 {
+  public static void main(String[] args) {
+    Point p1 = new Point(15, 14);
+    Point p2 = new Point(2, 2);
+    System.out.println("Расстояние между точками = " + p1.distance(p2));
+
+    //Для функции:
+    //System.out.println("Расстояние между точками = " + distance(p1,p2));
+  }
+
+  //Вызов Функции
+  //public static  double distance(Point p1,Point p2){
+    //return Math.sqrt((p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y)*(p2.y - p1.y));
+
+  }
+
